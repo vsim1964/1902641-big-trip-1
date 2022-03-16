c602156e342eba2e4324d02288247f36d0004901
AlexandraNedosekina <volk.sash10@gmail.com>
2022-03-17 02:22:07 +0500

    Шаблонизируй это

---
+++ src/view/add-event-item-view.js
@@ -0,0 +1,107 @@
+export const createAddEventItemTemplate = (tripEvent) => {
+  const {offers, description, photos} = tripEvent;
+  const eventType = 'check-in';
+  const templateDatetime = dayjs().add(14, 'day').hour(10).minute(0).format('DD/MM/YY HH:mm');
+  const createOfferElement = (offer) => {
+    const offerName = offer.name;
+    const offerPrice = offer.price;
+    const offerType = offer.type;
+    return `<div class="event__available-offers">
+                      <div class="event__offer-selector">
+                        <input class="event__offer-checkbox  visually-hidden" id="event-offer-${offerType}-1" type="checkbox" name="event-offer-${offerType}" >
+                        <label class="event__offer-label" for="event-offer-name-1">
+                          <span class="event__offer-title">${offerName}</span>
+                          &plus;&euro;&nbsp;
+                          <span class="event__offer-price">${offerPrice}</span>
+                        </label>
+                      </div>
+    `;
+  };
+  const addableOfferElements = offers.map(createOfferElement).join('');
+  const createAddableOfferList = (addableOffers) => {
+    if (addableOffers.length !== 0){
+      return `<section class="event__section  event__section--offers">
+                    <h3 class="event__section-title  event__section-title--offers">Offers</h3>
+                    ${addableOfferElements}
+                  </section>`;
+    }
+    return '';
+  };
+  const addableOfferList = createAddableOfferList(offers);
+  const translatePhotoToHTML = (photo) => (`<img className="event__photo" src="${photo}">`);
+  const photosList = photos.map(translatePhotoToHTML).join('');
+  const createLocationOption = (city) => (`<option value="${city}"></option>`);
+  const locationOptions = locations().map(createLocationOption).join('');
+  const createEventTypes = (types = eventTypes(), chosenEventType) => {
+    const createType = (currentType) => {
+      const isChecked = currentType === chosenEventType ? 'checked=""' : '';
+      const label = currentType.charAt(0).toUpperCase() + currentType.slice(1);
+      return `<div class="event__type-item">
+                          <input id="event-type-${currentType}-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="${currentType}" ${isChecked}>
+                          <label class="event__type-label  event__type-label--${currentType}" for="event-type-${currentType}-1">${label}</label>
+                        </div>`;
+    };
+    return types.map(createType).join('');
+  };
+  const eventTypesElements = createEventTypes(eventTypes(), eventType);
+  const eventTypeLabel = eventType.charAt(0).toUpperCase() + eventType.slice(1);
+  return `<li class="trip-events__item">
+              <form class="event event--edit" action="#" method="post">
+                <header class="event__header">
+                  <div class="event__type-wrapper">
+                    <label class="event__type  event__type-btn" for="event-type-toggle-1">
+                      <span class="visually-hidden">Choose event type</span>
+                      <img class="event__type-icon" width="17" height="17" src="img/icons/${eventType}.png" alt="Event type icon">
+                    </label>
+                    <input class="event__type-toggle  visually-hidden" id="event-type-toggle-1" type="checkbox">
+                    <div class="event__type-list">
+                      <fieldset class="event__type-group">
+                        <legend class="visually-hidden">Event type</legend>
+                        ${eventTypesElements}
+                      </fieldset>
+                    </div>
+                  </div>
+                  <div class="event__field-group  event__field-group--destination">
+                    <label class="event__label  event__type-output" for="event-destination-1">
+                      ${eventTypeLabel}
+                    </label>
+                    <input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value="" list="destination-list-1">
+                    <datalist id="destination-list-1">
+                      ${locationOptions}
+                    </datalist>
+                  </div>
+                  <div class="event__field-group  event__field-group--time">
+                    <label class="visually-hidden" for="event-start-time-1">From</label>
+                    <input class="event__input  event__input--time" id="event-start-time-1" type="text" name="event-start-time" value="${templateDatetime}">
+                    &mdash;
+                    <label class="visually-hidden" for="event-end-time-1">To</label>
+                    <input class="event__input  event__input--time" id="event-end-time-1" type="text" name="event-end-time" value="${templateDatetime}">
+                  </div>
+                  <div class="event__field-group  event__field-group--price">
+                    <label class="event__label" for="event-price-1">
+                      <span class="visually-hidden">Price</span>
+                      &euro;
+                    </label>
+                    <input class="event__input  event__input--price" id="event-price-1" type="text" name="event-price" value="">
+                  </div>
+                  <button class="event__save-btn  btn  btn--blue" type="submit">Save</button>
+                  <button class="event__reset-btn" type="reset">Cancel</button>
+                </header>
+                <section class="event__details">
+                  ${addableOfferList}
+                  <section class="event__section  event__section--destination">
+                    <h3 class="event__section-title  event__section-title--destination">Destination</h3>
+                    <p class="event__destination-description">${description}</p>
+                    <div class="event__photos-container">
+                      <div class="event__photos-tape">
+                        ${photosList}
+                      </div>
+                    </div>
+                  </section>
+                </section>
+              </form>
+            </li>`;
+};
+import dayjs from 'dayjs';
+import {locations} from '../mock/locations';
+import {eventTypes} from '../mock/event-types';
--- src/view/trip-events-item-view.js
+++ src/view/trip-events-item-view.js
@@ -1,40 +1,76 @@
-export const createTripEventsItemTemplate = () => (
-    `<li class="trip-events__item">
-                <li class="trip-events__item">
-                <div class="event">
-                  <time class="event__date" datetime="2019-03-18">MAR 18</time>
-                  <div class="event__type">
-                    <img class="event__type-icon" width="42" height="42" src="img/icons/taxi.png" alt="Event type icon">
-                  </div>
-                  <h3 class="event__title">Taxi Amsterdam</h3>
-                  <div class="event__schedule">
-                    <p class="event__time">
-                      <time class="event__start-time" datetime="2019-03-18T10:30">10:30</time>
-                      &mdash;
-                      <time class="event__end-time" datetime="2019-03-18T11:00">11:00</time>
-                    </p>
-                    <p class="event__duration">30M</p>
-                  </div>
-                  <p class="event__price">
-                    &euro;&nbsp;<span class="event__price-value">20</span>
-                  </p>
-                  <h4 class="visually-hidden">Offers:</h4>
-                  <ul class="event__selected-offers">
-                    <li class="event__offer">
-                      <span class="event__offer-title">Order Uber</span>
-                      &plus;&euro;&nbsp;
-                      <span class="event__offer-price">20</span>
-                    </li>
-                  </ul>
-                  <button class="event__favorite-btn event__favorite-btn--active" type="button">
-                    <span class="visually-hidden">Add to favorite</span>
-                    <svg class="event__favorite-icon" width="28" height="28" viewBox="0 0 28 28">
-                      <path d="M14 21l-8.22899 4.3262 1.57159-9.1631L.685209 9.67376 9.8855 8.33688 14 0l4.1145 8.33688 9.2003 1.33688-6.6574 6.48934 1.5716 9.1631L14 21z"/>
-                    </svg>
-                  </button>
-                  <button class="event__rollup-btn" type="button">
-                    <span class="visually-hidden">Open event</span>
-                  </button>
+export const createTripEventsItemTemplate = (tripEvent) => {
+
+  const {eventType, location, price, startDate, endDate, duration, offers, isFavorite} = tripEvent;
+  const startDay = dayjs(startDate).format('MMM D');
+  const beginDate = dayjs(startDate).format('YYYY-MM-DD');
+  const startTime = dayjs(startDate).format('HH:mm');
+  const startDatetime = dayjs(startDate).format('YYYY-MM-DDTHH:mm');
+  const endTime = dayjs(endDate).format('HH:mm');
+  const endDatetime = dayjs(endDate).format('YYYY-MM-DDTHH:mm');
+  const createOfferElement = (offer) => {
+    if (offer.isChosen) {
+      const offerName = offer.name;
+      const offerPrice = offer.price;
+      return `<li class="event__offer">
+                    <span class="event__offer-title">${offerName}</span>
+                    &plus;&euro;&nbsp;
+                    <span class="event__offer-price">${offerPrice}</span>
+                  </li>`;
+    }
+  };
+
+  const offersElements = offers.map(createOfferElement).join('');
+  const isFavoriteClass = isFavorite ? ' event__favorite-btn--active' : '';
+
+  const buildDurationString = (interval) => {
+    const result = [];
+    if (interval.days !== 0) {
+      result[0] = String(interval.days).padStart(2,'0');
+      result[0] += 'D';
+    }
+    if (interval.hours !== 0) {
+      result[1] = String(interval.hours).padStart(2,'0');
+      result[1] += 'H';
+    }
+    if (interval.minutes !== 0) {
+      result[2] = String(interval.minutes).padStart(2,'0');
+      result[2] += 'M';
+    }
+    return result.join(' ');
+  };
+  const durationString = buildDurationString(duration);
+
+  return `<li class="trip-events__item">
+              <li class="trip-events__item">
+              <div class="event">
+                <time class="event__date" datetime="${beginDate}">${startDay}</time>
+                <div class="event__type">
+                  <img class="event__type-icon" width="42" height="42" src="img/icons/${eventType}.png" alt="Event type icon">
                 </div>
-              </li>`
-  );
+                <h3 class="event__title">${eventType} ${location}</h3>
+                <div class="event__schedule">
+                  <p class="event__time">
+                    <time class="event__start-time" datetime="${startDatetime}">${startTime}</time>
+                    &mdash;
+                    <time class="event__end-time" datetime="${endDatetime}">${endTime}</time>
+                  </p>
+                  <p class="event__duration">${durationString}</p>
+                </div>
+                <p class="event__price">
+                  &euro;&nbsp;<span class="event__price-value">${price}</span>
+                </p>
+                <h4 class="visually-hidden">Offers:</h4>
+                <ul class="event__selected-offers">${offersElements}</ul>
+                <button class="event__favorite-btn${isFavoriteClass}" type="button">
+                  <span class="visually-hidden">Add to favorite</span>
+                  <svg class="event__favorite-icon" width="28" height="28" viewBox="0 0 28 28">
+                    <path d="M14 21l-8.22899 4.3262 1.57159-9.1631L.685209 9.67376 9.8855 8.33688 14 0l4.1145 8.33688 9.2003 1.33688-6.6574 6.48934 1.5716 9.1631L14 21z"/>
+                  </svg>
+                </button>
+                <button class="event__rollup-btn" type="button">
+                  <span class="visually-hidden">Open event</span>
+                </button>
+              </div>
+            </li>`;
+};
+import dayjs from 'dayjs';
